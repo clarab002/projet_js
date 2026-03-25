d1fea6e38efb422f483baee9970481c1f0f7c39a
ClaraD79 <clara.desgranges79@gmail.com>
2026-03-25 17:43:23 +0100

    loader

--- global.js
+++ global.js
@@ -1,5 +1,20 @@
 // header
 // menu + horloge + clic logo + navigation + loader + presentation equipe (demande)
+function delay(event){
+    let bla = setInterval(
+        function(){
+            console.log("chiant chiant chiant");
+        },1000
+    );
+    setTimeout(
+        function(){
+            clearInterval(bla);
+            console.log("mince");
+            let id = event.target.id;
+            window.location.href = `${id}.html`;
+        },2000
+    )
+}
 
 // footer
 // numero tel, validation,  sonnerie + adresses 
@@ -8,3 +23,11 @@
 
 // erreurs
 
+function main(){
+    document.getElementById("a_propos").addEventListener("click",delay);
+    document.getElementById("accueil").addEventListener("click",delay);
+    document.getElementById("contact").addEventListener("click",delay);
+    document.getElementById("presentation").addEventListener("click",delay);
+    document.getElementById("produit").addEventListener("click",delay);
+}
+main();
