73cb284ee38e9de7b25fc182d68e10efb5aea2df
ClaraD79 <clara.desgranges79@gmail.com>
2026-04-29 15:27:15 +0200

    plus de hover, un clic mtn

--- accueil/accueil.js
+++ accueil/accueil.js
@@ -1,3 +1,13 @@
 // survol/clic image = zoom x2
+function zoom(e){
+    console.log(e.target.classList);
+    e.target.classList.toggle("zoom");
+}
+
 
 // slogan 
+
+function main(){
+    document.getElementById("img-bann").addEventListener("click",zoom)
+}
+main();
