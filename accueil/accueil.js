0760e89c2a2c9ede4ebc120f3ffe006da8bfb4cb
ClaraD79 <clara.desgranges79@gmail.com>
2026-04-29 17:58:36 +0200

    début de slogan

--- accueil/accueil.js
+++ accueil/accueil.js
@@ -3,10 +3,26 @@ function zoom(e){
     e.target.classList.toggle("zoom");
 }
 
+const endroitSlogan = document.querySelector("#slogan");
 
 // slogan 
+function affichageSlogan(){
+    let slogan = ["Des", "histoires", "contées", "par", "nos", "plumes"]
+    console.log(slogan[1]);
+    let i = 0; 
+    let apparition = setInterval(function(){
+        endroitSlogan.innerText += " " + slogan[i];
+        i++; 
+    },1000)
+    setTimeout(function(){
+        clearInterval(apparition);
+    },6000)
+    endroitSlogan.style.transform = translate(70)
+}
+
 
 function main(){
     document.getElementById("img-bann").addEventListener("click",zoom)
+    affichageSlogan();
 }
 main();
